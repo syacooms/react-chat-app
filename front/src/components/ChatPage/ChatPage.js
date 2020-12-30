a018847938fc98fd4e43b6c23753a9b090947d09
syacooms <syacooms@gmail.com>
2020-12-30 15:38:28 +0900

    Create 메세지 기능

--- front/src/components/ChatPage/ChatPage.js
+++ front/src/components/ChatPage/ChatPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import SidePanel from './SidePanel/SidePanel';
 import MainPanel from './MainPanel/MainPanel';
+import { useSelector } from 'react-redux';
 
 function ChatPage() {
+  const currentChatRoom = useSelector(
+    (state) => state.chatRoom.currentChatRoom,
+  );
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ width: '300px' }}>
         <SidePanel />
       </div>
       <div style={{ width: '1200px' }}>
-        <MainPanel />
+        <MainPanel key={currentChatRoom && currentChatRoom.id} />
       </div>
     </div>
   );
